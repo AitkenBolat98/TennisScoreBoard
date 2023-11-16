4a04b4748d4067e97c66773f849a5354b969f294
Aitken Bolat <aitken.bolat@gmail.com>
2023-11-16 20:37:27 +0600

    16 nov

--- src/main/java/module/Matches.java
+++ src/main/java/module/Matches.java
@@ -14,17 +14,30 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Matches {
     @Id
-    @SequenceGenerator( name = "match_sequence",
-                        sequenceName = "match_sequence",
-                        allocationSize = 1)
-    @GeneratedValue( strategy = GenerationType.SEQUENCE,
-                     generator = "match_sequence")
+    @SequenceGenerator(name = "match_sequence",
+            sequenceName = "match_sequence",
+            allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "match_sequence")
     @Column(name = "id",
             updatable = false)
     private Long id;
-
+    @ManyToOne()
+    @JoinColumn(name = "player1_id",
+                nullable = false,
+                referencedColumnName = "id",
+                foreignKey = @ForeignKey(
+                        name = "player1_match_fk"
+                ))
     private Players player1;
 
+    @ManyToOne()
+    @JoinColumn(name = "player2_id",
+            nullable = false,
+            referencedColumnName = "id",
+            foreignKey = @ForeignKey(
+                    name = "player2_match_fk"
+            ))
     private Players player2;
 
     private Players winner;
